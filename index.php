012495d9f7c4ebb5126f35bd35d0148184fe35f4
Hajime Furusawa <hajimeken2@gmail.com>
2019-07-06 22:19:32 +0900

    野球のスコアボード
    PHP、HTML,CSSで作成

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+	<head>
+<link rel="stylesheet" href="test.css">
+    <title>試合結果</title>
+    </head>
+<body>
+<h1>自軍チーム名を入力してください</h1>
+<form action="score.php" method="post">
+<div class="team_name">
+	<label class="ef">
+	<input type="text" name="Ateam" placeholder="自軍チーム名を入力">
+	</label>
+</div>
+１回
+<div class="A">2回</div>
+<div class="B">3回</div>
+<div class="C">4回</div>
+<div class="D">5回</div>
+<div class="E">1回</div>
+<div class="F">2回</div>
+<div class="G">3回</div>
+<div class="H">4回</div>
+<div class="I">5回</div>
+
+<div class="kai">
+	<input type="text" style="width:35px; height:30px;" name="Ascore" size="15" maxlength="60">点
+</div>
+<div class="kai2">
+	<input type="text" style="width:35px; height:30px;" name="Bscore" size="15" maxlength="60">点
+</div>
+<div class="kai3">
+	<input type="text" style="width:35px; height:30px;" name="Cscore" size="15" maxlength="60">点
+</div>
+<div class="kai4">
+	<input type="text" style="width:35px; height:30px;" name="Dscore" size="15" maxlength="60">点
+</div>
+<div class="kai5">
+	<input type="text" style="width:35px; height:30px;" name="Escore" size="15" maxlength="60">点
+</div>
+<hr class="sen"></hr>
+<div class="kaiA">
+	<input type="text" style="width:35px; height:30px;" name="Fscore" size="15" maxlength="60">点
+</div>
+<div class="kaiB">
+	<input type="text" style="width:35px; height:30px;" name="Gscore" size="15" maxlength="60">点
+</div>
+<div class="kaiC">
+	<input type="text" style="width:35px; height:30px;" name="Hscore" size="15" maxlength="60">点
+</div>
+<div class="kaiD">
+	<input type="text" style="width:35px; height:30px;" name="Iscore" size="15" maxlength="60">点
+</div>
+<div class="kaiE">
+	<input type="text" style="width:35px; height:30px;" name="Jscore" size="15" maxlength="60">点
+</div>
+<div class="teki"><h1>敵軍チーム名を入力してください</h1></div>
+<div class="team_name2">
+	<label class="ef2">
+	<input type="text" name="Bteam" placeholder="敵軍チーム名を入力">
+	</label>
+</div>
+<br/>
+<br/>
+     <input type="submit" value="入力が終わったらクリック">
+</form>
+</body>
+</html>
---
+++ score.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<?php
+  $Ascore = $_POST['Ascore'];
+  $Bscore = $_POST['Bscore'];
+  $Cscore = $_POST['Cscore'];
+  $Dscore = $_POST['Dscore'];
+  $Escore = $_POST['Escore'];
+  $Ateam  = $_POST['Ateam'];
+
+  $Fscore = $_POST['Fscore'];
+  $Gscore = $_POST['Gscore'];
+  $Hscore = $_POST['Hscore'];
+  $Iscore = $_POST['Iscore'];
+  $Jscore = $_POST['Jscore'];
+  $Bteam  = $_POST['Bteam'];
+  
+  if ($Ateam === '' or $Bteam === '') {
+	echo 'チーム名が入力されていません';
+	exit;
+  }
+  
+  if ($Ascore === '' or $Bscore === ''or $Cscore === ''
+      or $Dscore === ''or $Escore === ''or $Fscore === ''
+      or $Gscore === ''or $Hscore === ''or $Iscore === ''
+      or $Jscore === '') {
+	echo '点数が入力されていない箇所があります';
+	exit;
+  }
+  
+  if (preg_match("/[^0-9^]/",$Ascore)){
+	echo '数字以外が入力されております';
+	exit;
+  }
+  
+  if (preg_match("/[^0-9^]/",$Bscore)){
+	echo '数字以外が入力されております';
+	exit;
+  }
+  
+  if (preg_match("/[^0-9^]/",$Cscore)){
+	echo '数字以外が入力されております';
+	exit;
+  }
+  
+   if (preg_match("/[^0-9^]/",$Dscore)){
+	echo '数字以外が入力されております';
+	exit;
+  }
+  
+  if (preg_match("/[^0-9^]/",$Escore)){
+	echo '数字以外が入力されております';
+	exit;
+  }
+  
+  if (preg_match("/[^0-9^]/",$Fscore)){
+	echo '数字以外が入力されております';
+	exit;
+  }
+  
+  if (preg_match("/[^0-9^]/",$Gscore)){
+	echo '数字以外が入力されております';
+	exit;
+  }
+  
+   if (preg_match("/[^0-9^]/",$Hscore)){
+	echo '数字以外が入力されております';
+	exit;
+  }
+  
+    if (preg_match("/[^0-9^]/",$Iscore)){
+	echo '数字以外が入力されております';
+	exit;
+  }
+  
+    if (preg_match("/[^0-9^]/",$Jscore)){
+	echo '数字(半角英数字)以外が入力されております';
+	exit;
+  }
+                                                                                             
+                       
+?>
+<div class="baseball game_score">
+<table class="game_box baseball times5">
+<tbody>
+<tr>
+</tr>
+<tr>
+</tr>
+<tr>
+<table border="2" style="border-collapse: collapse; border-color: red">
+<td class="game_cell_data team_name"><?=$Ateam ?></td>
+<td class="game_cell_data score"><?=$Ascore ?></td>
+<td class="game_cell_data score"><?=$Bscore ?></td>
+<td class="game_cell_data score"><?=$Cscore ?></td>
+<td class="game_cell_data score"><?=$Dscore ?></td>
+<td class="game_cell_data score"><?=$Escore ?></td>
+<td class="game_cell_data total_score"><?php echo $Ascore + $Bscore + $Cscore + $Dscore + $Escore ?></td><tr>
+
+<tr>
+<td class="game_cell_data team_name"><?=$Bteam ?></td>
+<td class="game_cell_data score"><?=$Fscore ?></td>
+<td class="game_cell_data score"><?=$Gscore ?></td>
+<td class="game_cell_data score"><?=$Hscore ?></td>
+<td class="game_cell_data score"><?=$Iscore ?></td>
+<td class="game_cell_data score"><?=$Jscore ?></td>
+<td class="game_cell_data total_score"><?php echo $Fscore + $Gscore + $Hscore + $Iscore + $Jscore ?> </td>
+</tr>
+</tbody>
+</table>
+
+<?php
+echo "<br />";
+if ( $Ascore + $Bscore + $Cscore + $Dscore + $Escore < $Fscore + $Gscore + $Hscore + $Iscore + $Jscore){
+   echo $Bteam."の勝利です。";
+}
+elseif ( $Ascore + $Bscore + $Cscore + $Dscore + $Escore > $Fscore + $Gscore + $Hscore + $Iscore + $Jscore){
+	echo $Ateam."の勝利です。";
+}
+
+else{
+echo "引き分けです。";
+}
+?>
+</div>
+</html>
